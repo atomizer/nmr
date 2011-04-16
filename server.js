cca513495c9c5d7fec23324ec8aa0d048eee21d6
atomizer <danila.gerasimov@gmail.com>
2011-04-17 03:50:20 +0400

    changes, woo

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var request = require('request');
 var util = require('util');
+var canvas = require('canvas');
 
 var s = http.createServer();
 s.on('request', function(req, res) {
 	res.writeHead('200', {'content-type': 'text/plain'});
-	res.end(util.inspect(request));
+	res.end(util.inspect(canvas) + '\n');
 });
 
-s.listen(8080);
+s.listen(80);
