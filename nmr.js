2e6db8baec1349ba8550df03178fa554a7c5b1ed
atomizer <danila.gerasimov@gmail.com>
2011-05-08 22:55:16 +0400

    this is stupid

--- nmr.js
+++ nmr.js
@@ -302,7 +302,7 @@ NMR.prototype.prepImage = function(urlf, blend, cb) {
 			if (--that.pending == 0) cb();
 			return;
 		}
-		fs.writeFile(filename, body, function() {
+		fs.writeFile(filename, body, 'binary', function() {
 			console.log('#', url, '=>', filename);
 			expandImages();
 		});
