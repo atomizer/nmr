d27a3717998e6638fa5829267fb7af6cf19e15cc
atomizer <danila.gerasimov@gmail.com>
2011-05-08 23:18:17 +0400

    last try

--- nmr.js
+++ nmr.js
@@ -302,7 +302,7 @@ NMR.prototype.prepImage = function(urlf, blend, cb) {
 			if (--that.pending == 0) cb();
 			return;
 		}
-		fs.writeFile(filename, body, 'binary', function() {
+		fs.writeFile(filename, body, 'ascii', function() {
 			console.log('#', url, '=>', filename);
 			expandImages();
 		});
